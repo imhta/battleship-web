1771f3e53b0267ee72cfc936c1541ce5162d6f4f
imhta <imhtapm@gmail.com>
2020-01-08 20:38:59 +0530

    :sparkles: :up:  update player for single responsibillity
    Co-Authored-By: mesi21 <mesi21@users.noreply.github.com>

--- src/lib/player.js
+++ src/lib/player.js
@@ -1,5 +1,20 @@
-const Player = () => {
-
-}
+const Player = enemyBoard => {
+  let totalMoves = 0;
+  const isWon = () => {
+    if (totalMoves >= enemyBoard.totalShipsLength) {
+      return enemyBoard.isAllSunk();
+    }
+    return false;
+  };
+  const incrementMove = () => {
+    totalMoves += 1;
+    return totalMoves;
+  };
+  return {
+    totalMoves,
+    isWon,
+    incrementMove,
+  };
+};
 
 export default Player;
