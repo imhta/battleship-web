ac5a5a9e35a4d23ccd6b26f3fc5973ae456de1aa
imhta <imhtapm@gmail.com>
2020-01-09 14:14:12 +0530

    :up: adding init of the lib and factory function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 import './style/style.scss';
+import HumanPlayer from './lib/humanPlayer';
+import GameBoard from './lib/gameboard';
+import ComputerPlayer from './lib/computerPlayer';
+import UIController from './controller/UI.controller';
 
-console.log('hi');
+const humanPlayerBoard = GameBoard();
+const computerPlayerBoard = GameBoard();
+
+const humanPlayer = HumanPlayer({ enemyBoard: computerPlayerBoard });
+const computerPlayer = ComputerPlayer({ enemyBoard: humanPlayerBoard });
+
+const UI = UIController({ humanPlayerBoard, computerPlayerBoard });
