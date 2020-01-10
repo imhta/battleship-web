1f77d3ceface7d8cf7e8994207ecd55be8535ae0
Molnar Emese <mse212002@gmail.com>
2020-01-10 16:14:00 +0200

    :white checkmark: Added test for player attack

--- src/__test__/players.test.js
+++ src/__test__/players.test.js
@@ -1,12 +1,27 @@
 /* eslint-disable no-undef */
 import HumanPlayer from '../lib/humanPlayer';
 import GameBoard from '../lib/gameboard';
+import Player from '../lib/player';
+import ComputerPlayer from '../lib/computerPlayer';
+
+it('check player move', () => {
+  const enemyBoard = GameBoard();
+  const player = Player({ enemyBoard });
+  expect(player.incrementMove()).toEqual(1);
+});
 
 it('check player attack', () => {
   const enemyBoard = GameBoard();
   const humanPlayer = HumanPlayer({ enemyBoard });
   humanPlayer.attack(5);
   expect(enemyBoard.everyAttack()[0]).toEqual(5);
+});
+
+it('check computer player attack', () => {
+  const enemyBoard = GameBoard();
+  const computerPlayer = ComputerPlayer({ enemyBoard });
+  computerPlayer.attack();
+  expect(enemyBoard.everyAttack().length).toEqual(1);
 });
 
 it('check player isWon', () => {
@@ -27,4 +42,3 @@ it('check player isWon true', () => {
     return false;
   });
 });
-
